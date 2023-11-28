5043bda2fef52c2f22db2bcd41eb5a648d66e570
Juan Camilo Herrera <juan8132juan@gmail.com>
2023-11-28 10:18:52 -0500

    Generado XML y peticion POST al back

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,13 +1,15 @@
 
-
 import { useState } from "react";
 import './basic.css'
+import axios from "axios";
 export default function Index() {
 
 
   const [endpoints, setEndpoints] = useState([
     { name: '', type: '' },
   ]);
+  const [nombre, setNombre] = useState('');
+  const [lenguaje, setLenguaje] = useState('');
   const addEndpoint = () => {
     setEndpoints([...endpoints, { name: '', type: '' }]);
   };
@@ -18,20 +20,65 @@ export default function Index() {
     newEndpoints[index][name] = value;
     setEndpoints(newEndpoints);
   };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    // Construir el XML
+    const xmlData = `
+      <data>
+        <name>${nombre}</name>
+        <lenguage>${lenguaje}</lenguage>
+        <endpoints>
+          ${endpoints.map(endpoint => ` <endpoint>
+              <name>${endpoint.name}</name>
+              <type>${endpoint.type}</type>
+            </endpoint>
+          `).join('')} </endpoints>
+      </data>
+    `;
+
+    console.log(xmlData)
+
+    // Realizar la solicitud POST con fetch
+    try {
+      const response = await axios.post('http://localhost:3000', xmlData, {
+        headers: {
+          'Content-Type': 'application/xml',
+        }
+      });
+
+      // Manejar la respuesta aquí
+      console.log('Respuesta:', response);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   return (
     <>
       <div className="Contenedor">
         <h2 className="Titulo">Generador de microservicios</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="basicos">
             <label>
               Nombre:
-              <input type="text" className="inputs" />
+              <input
+                type="text"
+                className="inputs"
+                value={nombre}
+                onChange={(e) => setNombre(e.target.value)}
+              />
             </label>
 
             <label>
               Lenguaje:
-              <input type="text" className="inputs" />
+              <input
+                type="text"
+                className="inputs"
+                value={lenguaje}
+                onChange={(e) => setLenguaje(e.target.value)}
+              />
             </label>
           </div>
 
